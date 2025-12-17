52fe6a5964ac0e2f9c99fc9547d8696ea1066b1f
ghada ahmed <ghadaahmed2019266@gmail.com>
2025-12-17 11:50:14 +0200

    login -register -client home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { Home } from './components/home/home';
-import { Hero } from './hero/hero';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:Home},
     {path:'register',component:Register},    
     {path:'login',component:Login},
-    {path:'hero',component:Hero},
    /* {path:'contact',component:Contact},
     {path:'menu',component:Menu},
     {path:'cart',component:Cart},
